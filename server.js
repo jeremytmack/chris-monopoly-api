ca2ef16c6f9eb6920de00eec40b2621e9d2d0de0
jeremytmack <jtmack@gmail.com>
2020-03-30 10:22:48 -0400

    Added readme and updated comments

--- server.js
+++ server.js
@@ -17,30 +17,38 @@ app.use(function(req, res, next) {
 
 app.use(express.json());
 
+// START A ROLL - Triggered from the "ROLL" button
 app.post("/startroll", (req, res) => {
   let isRolling = db.get("rolling").value();
   let reqData = req.body;
+  // Don't start a new roll if one is currently in progress (where rolling=true in db.json)
   if (!isRolling) {
     reqData.diceCount;
-    // create array of dice values
+    // create array of dice values.  If they sent a dice count of 3 dice, throw 3 dice on the board!
     let diceArray = [];
     for (let i = 0; i < reqData.diceCount; i++) {
+      // the magic!  Add a random dice number
       diceArray.push(getRandomNumber(1, 6));
     }
+    // Set user name, rolling status and dice values we created.
     db.set("rolling", true).write();
     db.set("user", reqData.user).write();
     db.set("diceArray", diceArray).write();
     res.json({ status: "success", diceArray: diceArray });
   } else {
-    res.json({ status: "error" });
+    res.json({ status: "dice roll already in progress." });
   }
 });
 
+// STOP ROLL is recieved from the UI after the dice finish their animation
 app.get("/stoproll", (req, res) => {
+  // update the db to set current roll to false.
   db.set("rolling", false).write();
   res.json({ status: "success" });
 });
 
+// CHECKROLL is how the UI checks to see if a roll is already in progress
+// we may be able to depricate this.
 app.get("/checkroll", (req, res) => {
   let rolling = db.get("rolling").value();
   let user = db.get("user").value();
@@ -54,6 +62,7 @@ app.get("/checkroll", (req, res) => {
   });
 });
 
+// generate random number
 function getRandomNumber(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -62,4 +71,5 @@ function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+// Start the server!
 app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
